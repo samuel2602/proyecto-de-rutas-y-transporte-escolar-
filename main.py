99efe63e904c8e8173c2dcbbfa8d7b7ee47843ce
samuel2602 <munozsamuel2602@gmail.com>
2026-09-16 08:18:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -28,11 +28,15 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allowed_origins = [
-    "https://monumental-faun-1cec9c.netlify.app",
-    "http://localhost:5173",
-    "http://127.0.0.1:5173"
-])
+    allow_origins=[
+        "https://monumental-faun-1cec9c.netlify.app",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.exception_handler(IntegrityError)
